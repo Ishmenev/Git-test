cec00c9822732fc22e382505749fb6a2954f7322
Ishmenev <ishmenevev@mail.ru>
2020-09-15 23:46:02 +0300

    one more change

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector('.btn')
 const def = document.querySelector('.def')
+const decor = document.querySelector('.decor')
 const passage = document.querySelector('.passage')
+const list = document.querySelector('.list')
+
 
 btn.addEventListener('click', (e) => {
   e.preventDefault()
@@ -12,4 +15,10 @@ def.addEventListener('click', (e) => {
   e.preventDefault()
 
   passage.style.color = 'black'
+})
+
+decor.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  list.style.border = '1px solid black'
 })
