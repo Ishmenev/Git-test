6dc3ab075f7ba255f32c7f01eb49c69809df0edd
Ishmenev <ishmenevev@mail.ru>
2020-09-15 23:35:23 +0300

    new actions added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('test')
+const btn = document.querySelector('.btn')
+const def = document.querySelector('.def')
+const passage = document.querySelector('.passage')
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  passage.style.color = 'tomato'
+})
+
+def.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  passage.style.color = 'black'
+})
